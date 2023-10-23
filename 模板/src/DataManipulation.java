56811d4f48ea848770baa4e7a9d10fa8cf51764f
aurora324 <126308766+aurora324@users.noreply.github.com>
2023-10-23 13:46:28 +0800

    模板

---
+++ 模板/src/DataManipulation.java
@@ -0,0 +1,9 @@
+public interface DataManipulation {
+
+    public int addOneMovie(String str);
+    public String allContinentNames();
+    public String continentsWithCountryCount();
+    public String FullInformationOfMoviesRuntime(int min, int max);
+    public String findMovieById(int id);
+    public String findMoviesByTitleLimited10(String title);
+}
