ef7f0ad449f5155126b9fb3c692b1a85bb0b6d88
aurora324 <126308766+aurora324@users.noreply.github.com>
2023-10-23 13:28:40 +0800

    Update Generation.java users文件能正常运行

--- Generation.java
+++ Generation.java
@@ -36,28 +36,147 @@ public class Generation {
     }
 
     public static void users() throws IOException {
-        File inputFile = new File("src/users.csv");
+        addUsers();
+        File inputFile = new File("src/addUsers.csv");
         File outputFile = new File("src/users.sql");
-        if(outputFile.exists()){
-            if(outputFile.delete()){
+        if (outputFile.exists()) {
+            if (outputFile.delete()) {
                 System.out.println("delete");
             }
-            if(outputFile.createNewFile()){
+            if (outputFile.createNewFile()) {
+                System.out.println("new file");
+            }
+        }
+        FileWriter fileWriter = new FileWriter(outputFile);
+
+
+        try (FileReader fileReader = new FileReader(inputFile);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            System.out.println("write");
+            String add;
+            while ((add = bufferedReader.readLine()) != null) {
+                add = add.replace('\'', ' ');
+                add = add.replace('[', '{');
+                add = add.replace(']', '}');
+                add = add.replace('"', ' ');
+                int indexOfSex = 0;
+                int index1 = 0;
+                int index2 = 0;
+                String[] arr = add.split(",");
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[i].equals("男") || arr[i].equals("女") || arr[i].equals("保密")) {
+                        indexOfSex = i;
+                    }
+
+
+                }
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[i].contains("{")) {
+                        index1 = i;
+                    }
+                }
+
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[i].contains("}")) {
+                        index2 = i;
+                    }
+                }
+
+
+
+                    fileWriter.write("INSERT INTO users (Mid,Name,Sex,Birthday,Level,Sign,following,identity) values (");
+                    //mid
+                    fileWriter.write(arr[0]);
+                    fileWriter.write(",");
+
+                    //name
+                    fileWriter.write("'");
+                    for (int i = 1; i <= indexOfSex - 1; i++) {
+                        fileWriter.write(arr[i]);
+                    }
+                    fileWriter.write("'");
+                    fileWriter.write(",");
+
+                    //sex
+                    fileWriter.write("'");
+                    fileWriter.write(arr[indexOfSex]);
+                    fileWriter.write("'");
+                    fileWriter.write(",");
+
+                    //birth
+                    fileWriter.write("'");
+                    fileWriter.write(arr[indexOfSex + 1]);
+                    fileWriter.write("'");
+                    fileWriter.write(",");
+
+                    //level
+                    fileWriter.write(arr[indexOfSex + 2]);
+                    fileWriter.write(",");
+
+                    //sign
+                    fileWriter.write("'");
+                    for (int i = indexOfSex + 3; i < index1; i++) {
+                        fileWriter.write(arr[i]);
+                    }
+                    fileWriter.write("'");
+                    fileWriter.write(",");
+
+                    //following
+                    fileWriter.write("'");
+                    for (int i = index1; i < index2; i++) {
+                        fileWriter.write(arr[i]+",");
+                    }
+                    fileWriter.write(arr[index2]);
+                    System.out.print(index1+" ");
+                    System.out.println(index2);
+                    fileWriter.write("'");
+                    fileWriter.write(",");
+
+                    fileWriter.write("'");
+                    fileWriter.write(arr[arr.length - 1]);
+                    fileWriter.write("'");
+                    fileWriter.write(");\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            fileWriter.flush();
+            fileWriter.close();
+        }
+    }
+
+    public static void addUsers() throws IOException {
+        File inputFile = new File("src/users.csv");
+        File outputFile = new File("src/addUsers.csv");
+        if (outputFile.exists()) {
+            if (outputFile.delete()) {
+                System.out.println("delete");
+            }
+            if (outputFile.createNewFile()) {
                 System.out.println("new file");
             }
         }
         FileWriter fileWriter = new FileWriter(outputFile);
         try (FileReader fileReader = new FileReader(inputFile);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            System.out.println("first write");
             bufferedReader.readLine();
-            System.out.println("write");
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                fileWriter.write("INSERT INTO users (Mid,Name,Sex,Birthday,Level,Sign,following,identity) values ("+line+");\n");
+            String add = bufferedReader.readLine();
+            while (!add.equals("122879,敖厂长,保密,,6,,[],user")) {
+                if (add.equals("")) {
+                    add = " ";
+                }
+                fileWriter.write(add);
+                if (add.endsWith("user")) {
+                    fileWriter.write("\n");
+                }
+                add = bufferedReader.readLine();
             }
+            fileWriter.write("122879,敖厂长,保密,,6,,[],user");
+            System.out.println("finish");
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             fileWriter.flush();
             fileWriter.close();
         }
